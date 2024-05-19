fcf5b976fe0ee173313d8121a434ed88cda09cf6
Prabeton <3846205@gmail.com>
2024-05-19 10:51:02 +0200

    api31

--- components/Card.tsx
+++ components/Card.tsx
@@ -9,10 +9,9 @@ type CardProps = {
 const Card: React.FC<CardProps> = ({ title, content, id }) => {
   return (
     <div className="flex flex-col items-center justify-center gap-6 p-4 bg-transparent border border-blue-300 shadow-xl w-[500px] rounded-xl ">
-      <Link href={`/posts/${id}`} passHref>
-        <h2 className="text-blue-600">{title}</h2>
-        <p>{content.substring(0, 100)}...</p>
-      </Link>
+      <h1 className="text-3xl font-bold text-center">
+        Witaj w Mojej Aplikacji!
+      </h1>
     </div>
   );
 };
